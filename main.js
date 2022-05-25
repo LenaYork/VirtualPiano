063fc3691cda1dfeffd71c8ace258658353dc111
LenaYork <lenayork@tut.by>
2022-05-25 13:17:44 +0300

    feat: add notes names change

--- main.js
+++ main.js
@@ -4,7 +4,25 @@ let isClicked = false;
 let activeButton = "notes";
 const buttons = document.querySelectorAll(".button");
 
+
+//buttons keys-notes 
 buttons.forEach(button => button.addEventListener("click", function(event) {
+
+    console.log(event.target.id);
+    const keys = document.querySelectorAll(".key");
+    const sharpKeys = document.querySelectorAll(".sharp-key"); 
+    if (event.target.id === "notes-button" && !event.target.classList.contains("active-button")) {
+        console.log("зашел в первое условие");
+        keys.forEach( key => key.classList.add("note"));
+        sharpKeys.forEach( key => key.classList.add("note"));
+    } else
+
+    if (event.target.id === "letter-button" && !event.target.classList.contains("active-button")) {
+        console.log("зашел во второе условие");
+        keys.forEach( key => key.classList.remove("note"));
+        sharpKeys.forEach( key => key.classList.remove("note"));
+    }
+    
     if (!event.target.classList.contains("active-button")) {
         buttons.forEach(button => button.classList.remove("active-button"));
         event.target.classList.add("active-button");
@@ -29,7 +47,7 @@ main.addEventListener("mouseup", function(event) {
 });
 
 main.addEventListener("mouseover", function(event) {
-    console.log(isClicked);
+    // console.log(isClicked);
     if ((event.target.classList.contains("key") 
         || event.target.classList.contains("sharp-key"))
         && isClicked) {
